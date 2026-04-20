30a9ae9a14e2e7b65c88370d54ef78e69c089b3b
Mostafa002 <mostafamadadi.1382@gmail.com>
2026-04-20 21:37:53 +0430

    change the content type to text/html

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((res,req)=>{
-    req.writeHead(200,{'content-type':'text/plain'});
-    req.end("helloworld");
+    req.writeHead(200,{'content-type':'text/html'});
+    req.end("<h1>hello world</h1>");
 });
 
 server.listen(3000,()=>{
