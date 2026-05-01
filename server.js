dfe8a077c95a1a36363a10e7f657a258de36f8d7
Mostafa002 <mostafamadadi.1382@gmail.com>
2026-05-01 23:46:48 +0430

    new changes to server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const http = require('http');
-const fs = require('fs');
-const { error } = require('console');
+const http = require("http");
+const fs = require("fs");
+const { error } = require("console");
 
-const server = http.createServer((req,res)=>{
-    fs.readFile("./message.txt",'utf-8',(error,data)=>{
-        if (error){
-            res.writeHead(500,{"Cotent-Type":"text/plain"});
-            res.end("error");
-            console.error(error);
-        }else{
-            res.writeHead(200,{"Content-Type":"text/html"});
-            res.end(`<p>${data}</p>`);
-        }
-
-    });
-});server.listen(3000,()=>{
-    console.log("the server is runnnig on http://localhost:3000");
+const server = http.createServer((req, res) => {
+  fs.readFile("./message.txt", "utf-8", (error, data) => {
+    if (error) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("error");
+      console.error(error);
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`<p>${data}</p>`);
+    }
+  });
+});
+server.listen(3000, () => {
+  console.log("the server is runnnig on http://localhost:3000");
 });
